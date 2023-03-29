9e36a293cf75384759ca27c8fbb6b3e85d4a204d
Doug Beckwith <dougcbeckwith@gmail.com>
2023-03-29 11:42:23 -0700

    start creating app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from "./components/Search";
+import Nav from "./components/Nav";
+import Photos from "./components/Photos";
+import "./App.css";
 
 function App() {
+  const navLinks = [
+    {
+      id: 1,
+      route: "cats",
+      title: "Cats"
+    },
+    {
+      id: 2,
+      route: "dogs",
+      title: "Dogs"
+    },
+    {
+      id: 3,
+      route: "birds",
+      title: "Birds"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search />
+      <Nav navLinks={navLinks} />
+      <Photos />
     </div>
   );
 }
