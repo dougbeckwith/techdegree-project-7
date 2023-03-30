66100dd66b4f1a184c2124ac96313bc61e4ec789
Doug Beckwith <dougcbeckwith@gmail.com>
2023-03-30 14:05:36 -0700

    navlinks data

---
+++ src/data/nav-links.js
@@ -0,0 +1,19 @@
+const navLinks = [
+  {
+    id: 1,
+    route: "cats",
+    title: "Cats"
+  },
+  {
+    id: 2,
+    route: "dogs",
+    title: "Dogs"
+  },
+  {
+    id: 3,
+    route: "birds",
+    title: "Birds"
+  }
+];
+
+export default navLinks;
